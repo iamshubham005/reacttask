c877794661d944b985db44d174e35db031631664
Ubuntu <ubuntu@ip-172-31-37-244.ap-south-1.compute.internal>
2025-07-05 17:42:55 +0000

    initial all push

--- Visualisation_Dashboard_Backend-main/visualisation_dashboard/urls.py
+++ Visualisation_Dashboard_Backend-main/visualisation_dashboard/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('data/', include('dashboard.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/data/', include('dashboard.urls')),
 ]
+
